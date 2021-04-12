4ba4e6e520f11a813a956dcc1466d12179672850
= <avinashkumarairforce@gmail.com>
2021-04-12 16:17:42 +0530

    cros changed to '*'

--- src/main/java/com/example/project/controller/DetailController.java
+++ src/main/java/com/example/project/controller/DetailController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/Detail")
 public class DetailController {
     @Autowired
